d8762c6bc5b34b7d36e6fb9dd8d01b41802b29cd
Md yasen rafe <mdyasenrafe@gmail.com>
2022-07-18 00:34:28 +0600

    project structuring working

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  // use fonts
+  const [loaded] = useFonts({
+    PoppinsRegular: require("./assets/Font/Poppins-Regular.ttf"),
+    PoppinsBold: require("./assets/Font/Gilroy-Bold.ttf"),
+    PoppinsSemiBold: require("./assets/Font/Gilroy-Medium.ttf"),
+  });
+  console.log(loaded);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider style={styles.container}>
+      <ImageBackground
+        source={require("./assets/get-started.png")}
+        // resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Welcome {"\n"} To FreshMart</Text>
+      </ImageBackground>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  text: {
+    fontFamily: "PoppinsBold",
+    paddingVertical: 16,
+    fontSize: 32,
+    textAlign: "center",
   },
 });
